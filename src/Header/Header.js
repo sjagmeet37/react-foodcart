bf3499fff3bde16ff9afecdaa3e941dd60cc20e0
Jagmeet Sandhu <sjagmeet37@gmail.com>
2023-07-28 00:25:03 +0530

    adding header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,5 +1,20 @@
+import React from "react";
+import style from "./Header.module.css"
+import meals from "./meals.jpg"
+
+
 const Header = () => {
 
+    return (
+    <React.Fragment>
+    <div className={style.header}>
+        <div>Meals</div>
+        <div>dummy cart</div>
+    </div>
+    <div className={style["main-image"]}>
+        <img src={meals} alt="Meals Photo"/>
+    </div>
+    </React.Fragment>);
 }
 
 export default Header;
