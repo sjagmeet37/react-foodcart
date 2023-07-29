b3a552a1e06b1dc59c2facbd6bb2a7f7b17bcc67
Jagmeet Sandhu <sjagmeet37@gmail.com>
2023-07-29 16:01:52 +0530

    button chnages in header and form

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import style from "./Header.module.css"
 import meals from "./../assets/meals.jpg"
+import Button from "../UI/Button/Button";
 
 
 const Header = () => {
@@ -8,8 +9,9 @@ const Header = () => {
     return (
     <React.Fragment>
     <div className={style.header}>
-        <div>Meals</div>
-        <div>dummy cart</div>
+        <h1>Meals</h1>
+        <div><Button type="button" name="Your Cart"
+        icon="true" badge={10}/></div>
     </div>
     <div className={style["main-image"]}>
         <img src={meals} alt="Meals"/>
--- src/Meal/MealForm/MealForm.js
+++ src/Meal/MealForm/MealForm.js
@@ -1,12 +1,12 @@
+import Button from "../../UI/Button/Button";
 import Input from "../../UI/Input/Input";
 
 const MealForm = (props) => {
 
     return (
         <div>
-           
-            <Input label="Amount" type="number"/>
-            <button>+Add</button>
+            <Input label="Amount" type="number" />
+            <Button type="button" name="+Add" />
         </div>
     );
 }
