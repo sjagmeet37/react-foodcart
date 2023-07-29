cc658a1f959cae02a1e2608f3b3f2eb2a59e5a43
Jagmeet Sandhu <sjagmeet37@gmail.com>
2023-07-29 17:09:22 +0530

    modal changes

---
+++ src/UI/Modal/Modal.js
@@ -0,0 +1,39 @@
+import styles from "./Modal.module.js";
+
+function Backdrop(props) {
+  return <div className={styles.backdrop}></div>;
+}
+
+function ModalOverlay(props) {
+  return (
+    <Card>
+      <div className={`${styles.modal}`}>
+        <div className={styles.header}>
+          <h2>{props.modalHeader}</h2>
+        </div>
+        <div className={styles.content}>{props.modalContent}</div>
+        <div className={styles.actions}>
+          <Button type="Button" name="Okay" onClk={props.showHideModal} />
+        </div>
+      </div>
+    </Card>
+  );
+}
+
+const Modal = (props) => {
+  return (
+    <React.Fragment>
+      ReactDOM.createRoot(
+      <Backdrop />, document.getElementById('backdrop-root'))
+      ReactDOM.createRoot(
+      <ModalOverlay
+        showHideModal={props.showHideModal}
+        modalHeader={props.modalHeader}
+        modalContent={props.modalContent}
+      />
+      , document.getElementById('overlay-root'))
+    </React.Fragment>
+  );
+};
+
+export default Modal;
