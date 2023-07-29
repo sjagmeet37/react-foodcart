b77d638e83576b7937dee503605105280f3d1627
Jagmeet Sandhu <sjagmeet37@gmail.com>
2023-07-29 11:51:18 +0530

    Meal summary

--- src/Meal/MealList/MealList.js
+++ src/Meal/MealList/MealList.js
@@ -1,13 +1,16 @@
 import Card from "../../UI/Card/Card";
 import MealItem from "../MealItem/MealItem";
-
+import style from "./MealList.module.css"
 const MealList = (props) => {
 
     return (
         <Card>
+            <div className={style.meals}>
             <MealItem />
             <MealItem />
             <MealItem />
+            </div>
+          
         </Card>
     );
 }
