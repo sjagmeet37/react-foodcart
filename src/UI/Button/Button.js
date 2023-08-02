4db9e81313251f140099d8b0be8000cdcf52b986
Jagmeet Sandhu <sjagmeet37@gmail.com>
2023-08-02 23:48:44 +0530

    fixing button click issue

--- src/UI/Button/Button.js
+++ src/UI/Button/Button.js
@@ -2,18 +2,18 @@ import style from "./Button.module.css";
 import cart from "./../../assets/shopping-cart.png";
 
 const Button = (props) => {
-  const onButtonClicked = (event) => {
-    props.onButtonClicked();
+
+    const onButtonClicked = (event) => {
+    if(props.onButtonClicked) {
+        props.onButtonClicked();
+      }    
   };
 
   const setClick =  {};
 
-  if(props.onButtonClicked) {
-    {onClick : props.onButtonClicked}
-  }
 
   return (
-    <button type={props.type} className={style.button} {...setClick}>
+    <button type={props.type} className={style.button} onClick={onButtonClicked}>
       {props.icon && <img src={cart} alt="cart" className={style.icon} />}
       {props.name}
       {props.badge && <span className={style.badge}> {props.badge} </span>}
